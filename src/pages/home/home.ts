5f765d72c4945d85242aa74fd2f4d0f023b7e8a0
Ritwick <d.ritwick24@gmail.com>
2019-06-21 10:02:43 -0700

    basic build for tracker

---
+++ src/pages/home/home.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { LocationTrackerProvider } from '../../providers/tracker/tracker';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, public locationTracker: LocationTrackerProvider) {
+
+  }
+
+
+
+  start(){
+    this.locationTracker.startTracking();
+  }
+
+  stop(){
+    this.locationTracker.stopTracking();
+  }
+
+}
